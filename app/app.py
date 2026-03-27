f23e4640aeb9e76689e8094d1011a6e0956a603f
marlabharghavsai <marlabharghavsai@gmail.com>
2026-03-27 09:15:49 +0000

    Fixed port isolation for proper blue-green deployment

--- app/app.py
+++ app/app.py
@@ -14,4 +14,5 @@ def health():
     return "OK", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    PORT = int(os.environ.get('APP_PORT', 5000))
+    app.run(host='0.0.0.0', port=PORT)
